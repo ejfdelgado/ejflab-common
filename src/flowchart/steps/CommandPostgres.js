dc6975ad39066a7990fe8010c83e3adc7e5e3e45
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2024-10-23 19:35:50 -0500

    developed content of command

--- src/flowchart/steps/CommandPostgres.js
+++ src/flowchart/steps/CommandPostgres.js
@@ -10,18 +10,37 @@ class CommandPostgres extends CommandBasic {
         const script = this.args[3];
         const postgresSrv = this.context.getSuperContext().getPostgresClient();
         if (action == "execute") {
+            //console.log("Starting execute...");
             const dbIdData = SimpleObj.getValue(this.context.data, dbIdPath, null);
             let bodyData = SimpleObj.getValue(this.context.data, bodyPath, null);
             bodyData = JSON.parse(JSON.stringify(bodyData));
             if (!(bodyData instanceof Array)) {
                 bodyData = [bodyData]
             }
-            for (let i = 0; i < bodyData.length; i++) {
-                const payload = JSON.parse(JSON.stringify(dbIdData));
-                payload.object = bodyData[i];
-                console.log(`script=${script}`);
-                console.log(JSON.stringify(payload, null, 4));
+            const now = new Date().getTime();
+
+            const pool = postgresSrv.getPool();
+            const client = await pool.connect();
+            await client.query("BEGIN");
+            const response = [];
+            try {
+                // Insert into media
+                for (let i = 0; i < bodyData.length; i++) {
+                    const payload = JSON.parse(JSON.stringify(dbIdData));
+                    payload.object = bodyData[i];
+                    payload.now = now;
+                    const result = await postgresSrv.executeFile(script, payload, client);
+                    response.push(result);
+                }
+                await client.query("COMMIT");
+            } catch (e) {
+                await client.query("ROLLBACK");
+                throw e;
+            } finally {
+                client.release();
             }
+            //console.log("Finishing execute...");
+            //console.log(JSON.stringify(response));
         }
     }
 }
